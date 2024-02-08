5c09cc7d9a260f5cd34679d9cb953c171008a3dc
Yago-0liveira <yago.olisilva.8@gmail.com>
2024-02-08 15:18:15 -0300

    ok

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const filmesRotas = require("./src/rotas/categoria");
+const filmesRotas = require("./src/rotas/filmes");
 const categoriaRotas = require("./src/rotas/categoria");
 
- const app = express();
- 
- app.use(bodyParser.json());
- const PORT = process.env.PORT || 3000; 
+const app = express();
 
- app.use("/filmes", filmesRotas);
- app.use("/categoria", categoriaRotas); 
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
+app.use("/filmes", filmesRotas);
+app.use("/categoria", categoriaRotas);
 
- app.listen(PORT, () => {
-    console.log('server running on http/localhost:${PORT}');
-
- })
+app.listen(PORT, () => {
+  console.log("server running on http/localhost:${PORT}");
+});
